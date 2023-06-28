1ed85cfc3aad351d77ea515f4a2e1ccfa8ac7e1e
Kenneth <2214kennyg@gmail.com>
2023-06-28 14:46:45 +1000

    first commit

---
+++ src/main/java/io/nology/eventcalendarspring/EventcalendarspringApplication.java
@@ -0,0 +1,13 @@
+package io.nology.eventcalendarspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventcalendarspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventcalendarspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nology/eventcalendarspring/EventcalendarspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.eventcalendarspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventcalendarspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
